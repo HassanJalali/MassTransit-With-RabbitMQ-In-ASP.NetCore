bb3a9d260c8efe3811857ce4a55bbd4c1bb273e9
h.jalali <h.jalali@JALALI-H>
2022-11-02 12:05:01 +0330

    Implement MassTransit With RabbitMQ In ASP.NetCore

---
+++ Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using MassTransit;
+using Newtonsoft.Json;
+using SharedModels;
+using System.Text.Json.Serialization;
+
+var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.ReceiveEndpoint("order-created-event", e =>
+    {
+        e.Consumer<OrderCreatedConsumer>();
+    });
+});
+
+await busControl.StartAsync(new CancellationToken());
+try
+{
+    Console.WriteLine("Press enter to exit");
+    await Task.Run(() => Console.ReadLine());
+}
+finally
+{
+    await busControl.StopAsync();
+}
+
+
+public class OrderCreatedConsumer : IConsumer<OrderCreated>
+{
+    public async Task Consume(ConsumeContext<OrderCreated> context)
+    {
+        var jsonMessage = JsonConvert.SerializeObject(context.Message);
+
+        Console.WriteLine($"Order Created Message : {jsonMessage}");
+    }
+}
---
+++ Producer/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using SharedModels;
+
+namespace Producer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : Controller
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public OrdersController(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(OrderDto orderDto)
+        {
+            await _publishEndpoint.Publish<OrderCreated>(new
+            {
+                Id = 1,
+                orderDto.ProductName,
+                orderDto.Price,
+                orderDto.Quantity,
+            });
+
+            return Ok();
+        }
+    }
+}
